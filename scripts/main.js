8aeebe841ef016f76ded53d804483ddc44df0bea
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-05-21 01:33:56 -0700

    test js fires upon hamburger toggle

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-/* blah */
+document.addEventListener("DOMContentLoaded", function() {
+    var checkbox = document.querySelector('.hamburger-checkbox');
+    
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked) {
+        console.log("Checkbox is checked");
+      } else {
+        console.log("Checkbox is unchecked");
+      }
+    });
+  });
+  
