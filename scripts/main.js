4626ac46f27dde2e6aa49177ee0d02252be76663
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-05-23 03:23:07 -0700

    changed side div to nav

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
   var hamburgerButton = document.querySelector('.hamburger-checkbox ~ label');
 
   checkbox.addEventListener('change', function() {
+    var label = this.nextElementSibling;
+  
     if (checkbox.checked) {
       adminPanel.classList.add('collapsed');
-      hamburgerButton.style.transform = 'rotate(180deg)';
+      label.classList.add('collapsed');
+      /* hamburgerButton.style.transform = 'rotate(180deg)'; */
     } else {
       adminPanel.classList.remove('collapsed');
-      hamburgerButton.style.transform = 'rotate(0deg)';
+      label.classList.remove('collapsed');
+      /* hamburgerButton.style.transform = 'rotate(0deg)'; */
     }
   });
 });
