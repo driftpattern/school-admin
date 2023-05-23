a6e5792d1e6bee1a88eacd4fd6f9ff2fdaeeb1d4
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-05-23 01:55:33 -0700

    working on css animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var checkbox = document.querySelector('.hamburger-checkbox');
   var adminPanel = document.querySelector('.admin-panel');
-  var adminLinks = document.querySelector('.admin-links');
-  var myAccount = document.querySelector('.my-account');
- 
+  var hamburgerButton = document.querySelector('.hamburger-checkbox ~ label');
+
   checkbox.addEventListener('change', function() {
     if (checkbox.checked) {
       adminPanel.classList.add('collapsed');
-      adminLinks.classList.add("collapsed");
-      myAccount.classList.add("collapsed");
+      hamburgerButton.style.transform = 'rotate(180deg)';
     } else {
       adminPanel.classList.remove('collapsed');
-      adminLinks.classList.remove("collapsed");
-      myAccount.classList.remove("collapsed");
+      hamburgerButton.style.transform = 'rotate(0deg)';
     }
   });
 });
