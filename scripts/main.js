93e66f726cf56a97f925d364ad8f4365e134b104
justin@ThinkCentre-01 <driftpattern@gmail.com>
2023-05-22 23:58:40 -0700

    cleanup to stylings

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,18 @@
-
 document.addEventListener("DOMContentLoaded", function() {
-    var checkbox = document.querySelector('.hamburger-checkbox');
-    var adminPanel = document.querySelector('.admin-panel');
-    var adminLinks = document.querySelector('.admin-links');
-    var myAccount = document.querySelector('.my-account');
-   
-    checkbox.addEventListener('change', function() {
-      if (checkbox.checked) {
-        adminPanel.classList.add('collapsed');
-        adminLinks.classList.add("collapsed");
-        myAccount.classList.add("collapsed");
-      } else {
-        adminPanel.classList.remove('collapsed');
-        adminLinks.classList.remove("collapsed");
-        myAccount.classList.remove("collapsed");
-      }
-    });
+  var checkbox = document.querySelector('.hamburger-checkbox');
+  var adminPanel = document.querySelector('.admin-panel');
+  var adminLinks = document.querySelector('.admin-links');
+  var myAccount = document.querySelector('.my-account');
+ 
+  checkbox.addEventListener('change', function() {
+    if (checkbox.checked) {
+      adminPanel.classList.add('collapsed');
+      adminLinks.classList.add("collapsed");
+      myAccount.classList.add("collapsed");
+    } else {
+      adminPanel.classList.remove('collapsed');
+      adminLinks.classList.remove("collapsed");
+      myAccount.classList.remove("collapsed");
+    }
   });
-
+});
